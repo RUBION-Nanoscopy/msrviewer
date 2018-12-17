5e6cb521c40550b069f7974ef45b27a059206a5f
patta42 <patrick.happel@rub.de>
2018-12-17 08:18:56 +0100

    Added functionality for transparency

--- +msrviewer/+icons/get_breeze_icon.m
+++ +msrviewer/+icons/get_breeze_icon.m
@@ -1,6 +1,32 @@
 function cdata = get_breeze_icon( icon, varargin )
-    d = double(imread(['+msrviewer/+icons/' icon '.png']))/255;
+    [a,~,alpha] =  imread(['+msrviewer/+icons/' icon '.png']);
+    d = double(a)/255;
     cdata = zeros(size(d,1), size(d,2), 3);
-    cdata(:,:,1) = d;
-    cdata(:,:,2) = d;
-    cdata(:,:,3) = d;
+
+    bg = phutils.gui.getDefaultPanelBG();
+    
+    if size(d,3) > 1
+        r = d(:,:,1);
+        g = d(:,:,2);
+        b = d(:,:,3);
+    else
+        r = d;
+        g = d;
+        b = d;
+    end
+    if ~isempty(alpha)
+        alpha = double(alpha)/255;
+        r = r.*alpha + (1-alpha)*bg(1);
+        g = g.*alpha + (1-alpha)*bg(2);
+        b = b.*alpha + (1-alpha)*bg(3);
+    else
+        % Fake transparency
+        r(r == 1) = bg(1);
+        g(g == 1) = bg(2);
+        b(b == 1) = bg(3);
+    end
+    cdata(:,:,1) = r;
+    cdata(:,:,2) = g;
+    cdata(:,:,3) = b;
+    
+    
